bf4a70aedef48fd10b893e29312ea56784998278
Wouter de Bie <wouter.de.bie@datadoghq.com>
2020-11-20 10:28:15 -0600

    Move to waitress for serving

--- locast4plex.py
+++ locast4plex.py
@@ -12,6 +12,8 @@ import os
 from utils import Configuration
 from plex import PlexHTTPServer
 from ssdp import SSDPServer
+import waitress
+from paste.translogger import TransLogger
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
@@ -26,6 +28,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 @click.option('-u', '--uuid', type=click.STRING, help='UUID', metavar='UUID', required=True)
 @click.option('-b', '--bind', 'bind_address', default="0.0.0.0", show_default=True, help='Bind IP address', metavar='IP_ADDR', )
 @click.option('-p', '--port', default=6077, show_default=True, help='Bind tcp port', metavar='PORT')
+@click.option('-v', '--verbose', is_flag=True, help='Enable verbse logging')
 @optgroup.group('Location overrides', cls=MutuallyExclusiveOptionGroup)
 @optgroup.option('--override-location', type=str, help='Override location', metavar="LAT,LONG")
 @optgroup.option('--override-zipcode', type=str, help='Override zipcode', metavar='ZIP')
@@ -49,7 +52,7 @@ def cli(*args, **config):
         geos = [locast.Geo(c.zipcode)]
     elif c.regions_file:
         geos = [locast.Geo(zipcode=z.rstrip())
-                for z in c.regions_file.readlines()]
+                for z in c.regions_file.readlines() if not z.startswith("#")]
     else:
         geos = [locast.Geo()]
 
@@ -66,12 +69,18 @@ def cli(*args, **config):
 
         port = c.port + i
         uuid = f"{c.uuid}_{i}"
+
         # Start Flask app on separate thread
-        plex_http_server = PlexHTTPServer(c, uuid, locast_service)
-        threading.Thread(target=plex_http_server.run,
+        app = PlexHTTPServer(c, uuid, locast_service)
+
+        if c.verbose:
+            app = TransLogger(app)
+
+        threading.Thread(target=waitress.serve,
+                         args=(app,),
                          kwargs={
                              'host': c.bind_address,
-                             'port': port, 'passthrough_errors': True}).start()
+                             'port': port}).start()
 
         ssdp = SSDPServer()
         ssdp.register('local', f'uuid:{uuid}::upnp:rootdevice',
