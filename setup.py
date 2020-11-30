fa358aa4105acfbbe1126d226e8009abdba44f8a
Wouter de Bie <wouter.de.bie@datadoghq.com>
2020-11-29 18:19:36 -0600

    Prep for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
-from setuptools import setup
+from os import path
+
+from setuptools import setup, setuptools
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='locast4plex',
     version='0.1.0',
+    description='locast to Plex Media Server integration',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wouterdebie/locast4plex",
+    author="Wouter de Bie",
+    author_email="pypi@evenflow.nl",
+    license="MIT",
     py_modules=['locast4plex'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
     install_requires=[
         'click',
         'click>=7.1.2',
