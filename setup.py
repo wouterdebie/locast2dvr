9dd5c5bfe68008c8dac2a3002575a06b0bcf7235
Wouter de Bie <wouter.de.bie@datadoghq.com>
2021-01-05 14:18:02 -0600

    Bump version: 0.4.32 → 0.4.33

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 setup(
     name='locast2dvr',
-    version='0.4.32',
+    version='0.4.33',
     description='locast to Plex Media Server/Emby integration',
     long_description=long_description,
     long_description_content_type="text/markdown",
